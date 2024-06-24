fa951dc4c9249db372667fb6dc1cb3a740c2f6d5
Irsyad Ridho <irsyadridho.r@gmail.com>
2024-06-25 00:35:47 +0700

    detail product full responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,24 @@ const tailwindConfig = {
           'custom-2': 'calc((100vw - 2560px)/2)',
         },
         screens: {
-          '1380': '1380px',
-          '470': '470px'
+          'ss': '375px',
+          's': '425px',
+          '2s': '440px',
+          '2ss': '520px',
+          'sm':	'640px',
+          'md':	'768px',
+          'lg':	'1024px',
+          'xl':	'1280px',
+          '1.5xl': '1380px',
+          '2xl':	'1536px',
         },
         colors: {             
           redcustom: '#D00000',
           orangecustom: '#FF8906',
           grayfooter: '#F8F8F8',
+        },
+        gridTemplateRows: {
+          '8-custom': 'repeat(8, minmax(0, auto))',
         },
       },
     },
